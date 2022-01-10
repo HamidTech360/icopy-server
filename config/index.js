ab5c8a62a78919aebb3f15f5720ab4204f58e4c9
HamidTech360 <hammedowolabi2001@gmail.com>
2022-01-09 22:31:44 -0800

    preparing app for deploayment

--- config/index.js
+++ config/index.js
@@ -1,4 +1,14 @@
 module.exports = (()=>{
+
+    const env = process.env.NODE_ENV
+    if(env == "production"){
+        return{
+            DATABASE_URL:'mongodb://localhost/icopy-prod',
+            JWT_SECRET:'12345',
+            EMAIL:'owolabihammed2001@gmail.com',
+            PASSWORD:'olalekan2019...'
+        }
+    }
     return{
         DATABASE_URL:'mongodb://localhost/icopy',
         JWT_SECRET:'12345',
