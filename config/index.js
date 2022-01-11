6cce18f56a4039142a22bab8b68992473c94b19b
HamidTech360 <hammedowolabi2001@gmail.com>
2022-01-11 00:13:24 -0800

    email issue

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = (()=>{
         DATABASE_URL:'mongodb://localhost/icopy',
         JWT_SECRET:'12345',
         PORT:3001,
-        EMAIL:'owolabihammed2001@gmail.com',
-        PASSWORD:'olalekan2019...'
+        EMAIL:'hamid@gtarealestatealade.com.ng',
+        PASSWORD:'olalekan2019'
     }
 })()
