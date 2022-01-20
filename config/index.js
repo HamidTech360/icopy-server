e32626f90e079b7bde663d06016ed3c605f99206
HamidTech360 <hammedowolabi2001@gmail.com>
2022-01-20 09:03:44 -1100

    email config

--- config/index.js
+++ config/index.js
@@ -5,18 +5,24 @@ module.exports = (()=>{
         return{
             DATABASE_URL:process.env.DATABASE_URL,
             JWT_SECRET:process.env.JWT_SECRET,
-            EMAIL:'hamid@gtarealestatealade.com.ng',
+            EMAIL:'owolabihammed2001@gmail.com',
             PASSWORD:'olalekan2019',
             PORT:process.env.PORT,
-            
-            
+            REFRESH_TOKEN:'1//04neDqBgx59tzCgYIARAAGAQSNwF-L9IriA2mpeDPKBLvptPTv6r_pML-OBkHD-zqc-WNaj8FqOpz-2ADLS6LQ5EBOtjLNKAAOok',
+            ACCESS_TOKEN:'ya29.A0ARrdaM9Nj0ZnO2QBRpLkU2irTM7WTF92m4dwv2JHkHfYMNt2GvdkaaYtsiMzVm_5JeKPBr-2oKp13azeZXISrk3Ezjsdo2cXygUrzwSfWIetw39Sbc_BMbaEtsspTG0mJUjq3yP0P0SctSY9VL-yYsqbp2pS',
+            CLIENT_ID:'140404377298-rirn46mh7o8nt0ga3vq7ka105pmsd9jd.apps.googleusercontent.com',
+            CLIENT_SECRET:'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'
         }
     }
     return{
         DATABASE_URL:'mongodb://localhost/icopy',
         JWT_SECRET:'12345',
         PORT:3001,
-        EMAIL:'hamid@gtarealestatealade.com.ng',
-        PASSWORD:'olalekan2019'
+        EMAIL:'owolabihammed2001@gmail.com',
+        PASSWORD:'olalekan2019',
+        REFRESH_TOKEN:'1//04neDqBgx59tzCgYIARAAGAQSNwF-L9IriA2mpeDPKBLvptPTv6r_pML-OBkHD-zqc-WNaj8FqOpz-2ADLS6LQ5EBOtjLNKAAOok',
+        ACCESS_TOKEN:'ya29.A0ARrdaM9Nj0ZnO2QBRpLkU2irTM7WTF92m4dwv2JHkHfYMNt2GvdkaaYtsiMzVm_5JeKPBr-2oKp13azeZXISrk3Ezjsdo2cXygUrzwSfWIetw39Sbc_BMbaEtsspTG0mJUjq3yP0P0SctSY9VL-yYsqbp2pS',
+        CLIENT_ID:'140404377298-rirn46mh7o8nt0ga3vq7ka105pmsd9jd.apps.googleusercontent.com',
+        CLIENT_SECRET:'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'
     }
 })()
--- controllers/mail.js
+++ controllers/mail.js
@@ -20,25 +20,67 @@ exports.sendMail = async (req, res)=>{
         const find_email = await AdminModel.find()
         const admin_email = find_email[0].email
 
-        
-        let transporter = nodemailer.createTransport({
-            service:'gmail',
-            auth:{
-              user: config.EMAIL,
-              pass: config.PASSWORD
+
+        const user_name     = config.EMAIL
+        const refresh_token = config.REFRESH_TOKEN;
+        const access_token  = config.ACCESS_TOKEN;
+        const client_id     = config.CLIENT_ID;
+        const client_secret = config.CLIENT_SECRET;
+    
+        const email_to = 'hammedowolabi2001@gmail.com';
+    
+        // const nodemailer = require('nodemailer');
+    
+        let transporter = nodemailer
+        .createTransport({
+            service: 'Gmail',
+            auth: {
+                type: 'OAuth2',
+                clientId: client_id,
+                clientSecret: client_secret
             }
-          })
+        });
+        transporter.on('token', token => {
+            console.log('A new access token was generated');
+            console.log('User: %s', token.user);
+            console.log('Access Token: %s', token.accessToken);
+            console.log('Expires: %s', new Date(token.expires));
+        });
+        // setup e-mail data with unicode symbols
+        let mailOptions = {
+            from    : user_name, // sender address
+            to      : email_to, // list of receivers
+            subject : 'Hello ✔', // Subject line
+            text    : 'Hello world ?', // plaintext body
+            html    : '<b>Hello Haamid ?</b>', // html body
+    
+            auth : {
+                user         : user_name,
+                refreshToken : refresh_token,
+                accessToken  : access_token,
+                expires      : 1494388182480
+            }
+        };
+
+        
+      //   let transporter = nodemailer.createTransport({
+      //       service:'gmail',
+      //       auth:{
+      //         user: config.EMAIL,
+      //         pass: config.PASSWORD
+      //       }
+      //     })
 
           
-      let mailOptions = {
-        from:'ICopy Story Blog',
-        to: admin_email,
-        subject: `${req.body.name} sent you an email`,
-        html:`<div>
-                  ${req.body.mailBody}
-                  <div>connect with me at ${req.body.email}</div>
-              </div>`
-      }
+      // let mailOptions = {
+      //   from:'ICopy Story Blog',
+      //   to: admin_email,
+      //   subject: `${req.body.name} sent you an email`,
+      //   html:`<div>
+      //             ${req.body.mailBody}
+      //             <div>connect with me at ${req.body.email}</div>
+      //         </div>`
+      // }
 
 
       transporter.sendMail(mailOptions, async function(err, data){
